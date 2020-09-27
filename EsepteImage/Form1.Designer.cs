95068d89dbd2e87090455438bc05b3ce596f4dec
Kushtar <eseptehelp@gmail.com>
2020-09-27 20:56:13 +0600

    йasd

--- EsepteImage/Form1.Designer.cs
+++ EsepteImage/Form1.Designer.cs
@@ -42,29 +42,38 @@
             this.tabControl2 = new System.Windows.Forms.TabControl();
             this.tabPage3 = new System.Windows.Forms.TabPage();
             this.label1 = new System.Windows.Forms.Label();
+            this.tabPage4 = new System.Windows.Forms.TabPage();
+            this.label7 = new System.Windows.Forms.Label();
+            this.tabPage5 = new System.Windows.Forms.TabPage();
+            this.label8 = new System.Windows.Forms.Label();
+            this.tabPage6 = new System.Windows.Forms.TabPage();
+            this.label9 = new System.Windows.Forms.Label();
+            this.tabPage7 = new System.Windows.Forms.TabPage();
+            this.label10 = new System.Windows.Forms.Label();
+            this.tabPage8 = new System.Windows.Forms.TabPage();
+            this.label11 = new System.Windows.Forms.Label();
+            this.tabPage9 = new System.Windows.Forms.TabPage();
+            this.label12 = new System.Windows.Forms.Label();
+            this.tabPage10 = new System.Windows.Forms.TabPage();
+            this.label13 = new System.Windows.Forms.Label();
             this.panel2 = new System.Windows.Forms.Panel();
+            this.label14 = new System.Windows.Forms.Label();
             this.label6 = new System.Windows.Forms.Label();
             this.label5 = new System.Windows.Forms.Label();
             this.progressBar1 = new System.Windows.Forms.ProgressBar();
-            this.tabPage4 = new System.Windows.Forms.TabPage();
-            this.tabPage5 = new System.Windows.Forms.TabPage();
-            this.tabPage6 = new System.Windows.Forms.TabPage();
-            this.tabPage7 = new System.Windows.Forms.TabPage();
-            this.tabPage8 = new System.Windows.Forms.TabPage();
-            this.tabPage9 = new System.Windows.Forms.TabPage();
-            this.tabPage10 = new System.Windows.Forms.TabPage();
-            this.label7 = new System.Windows.Forms.Label();
-            this.label8 = new System.Windows.Forms.Label();
-            this.label9 = new System.Windows.Forms.Label();
             this.tabControl1.SuspendLayout();
             this.tabPage1.SuspendLayout();
             this.panel1.SuspendLayout();
             this.tabControl2.SuspendLayout();
             this.tabPage3.SuspendLayout();
-            this.panel2.SuspendLayout();
             this.tabPage4.SuspendLayout();
             this.tabPage5.SuspendLayout();
             this.tabPage6.SuspendLayout();
+            this.tabPage7.SuspendLayout();
+            this.tabPage8.SuspendLayout();
+            this.tabPage9.SuspendLayout();
+            this.tabPage10.SuspendLayout();
+            this.panel2.SuspendLayout();
             this.SuspendLayout();
             // 
             // tabControl1
@@ -156,6 +165,7 @@
             // 
             // panel1
             // 
+            this.panel1.Controls.Add(this.tabControl2);
             this.panel1.Controls.Add(this.tabControl1);
             this.panel1.Controls.Add(this.button1);
             this.panel1.Controls.Add(this.label3);
@@ -175,10 +185,10 @@
             this.tabControl2.Controls.Add(this.tabPage8);
             this.tabControl2.Controls.Add(this.tabPage9);
             this.tabControl2.Controls.Add(this.tabPage10);
-            this.tabControl2.Location = new System.Drawing.Point(35, 22);
+            this.tabControl2.Location = new System.Drawing.Point(4, 215);
             this.tabControl2.Name = "tabControl2";
             this.tabControl2.SelectedIndex = 0;
-            this.tabControl2.Size = new System.Drawing.Size(1284, 562);
+            this.tabControl2.Size = new System.Drawing.Size(1144, 457);
             this.tabControl2.TabIndex = 11;
             this.tabControl2.Visible = false;
             // 
@@ -186,11 +196,10 @@
             // 
             this.tabPage3.AutoScroll = true;
             this.tabPage3.Controls.Add(this.label1);
-            this.tabPage3.Controls.Add(this.panel2);
             this.tabPage3.Location = new System.Drawing.Point(4, 26);
             this.tabPage3.Name = "tabPage3";
             this.tabPage3.Padding = new System.Windows.Forms.Padding(3);
-            this.tabPage3.Size = new System.Drawing.Size(1276, 532);
+            this.tabPage3.Size = new System.Drawing.Size(1136, 427);
             this.tabPage3.TabIndex = 0;
             this.tabPage3.Text = "2d планировака";
             this.tabPage3.UseVisualStyleBackColor = true;
@@ -205,102 +214,16 @@
             this.label1.Text = "Показано 0 из 0";
             this.label1.Visible = false;
             // 
-            // panel2
-            // 
-            this.panel2.Controls.Add(this.label6);
-            this.panel2.Controls.Add(this.label5);
-            this.panel2.Controls.Add(this.progressBar1);
-            this.panel2.Location = new System.Drawing.Point(329, 137);
-            this.panel2.Name = "panel2";
-            this.panel2.Size = new System.Drawing.Size(608, 248);
-            this.panel2.TabIndex = 0;
-            // 
-            // label6
-            // 
-            this.label6.AutoSize = true;
-            this.label6.Location = new System.Drawing.Point(217, 204);
-            this.label6.Name = "label6";
-            this.label6.Size = new System.Drawing.Size(13, 17);
-            this.label6.TabIndex = 2;
-            this.label6.Text = "-";
-            // 
-            // label5
-            // 
-            this.label5.AutoSize = true;
-            this.label5.Location = new System.Drawing.Point(113, 204);
-            this.label5.Name = "label5";
-            this.label5.Size = new System.Drawing.Size(87, 17);
-            this.label5.TabIndex = 1;
-            this.label5.Text = "Распознано";
-            // 
-            // progressBar1
-            // 
-            this.progressBar1.Location = new System.Drawing.Point(113, 157);
-            this.progressBar1.Name = "progressBar1";
-            this.progressBar1.Size = new System.Drawing.Size(413, 23);
-            this.progressBar1.TabIndex = 0;
-            // 
             // tabPage4
             // 
             this.tabPage4.Controls.Add(this.label7);
             this.tabPage4.Location = new System.Drawing.Point(4, 26);
             this.tabPage4.Name = "tabPage4";
             this.tabPage4.Padding = new System.Windows.Forms.Padding(3);
-            this.tabPage4.Size = new System.Drawing.Size(1276, 532);
+            this.tabPage4.Size = new System.Drawing.Size(1136, 427);
             this.tabPage4.TabIndex = 1;
             this.tabPage4.Text = "документ";
             this.tabPage4.UseVisualStyleBackColor = true;
-            // 
-            // tabPage5
-            // 
-            this.tabPage5.Controls.Add(this.label8);
-            this.tabPage5.Location = new System.Drawing.Point(4, 26);
-            this.tabPage5.Name = "tabPage5";
-            this.tabPage5.Size = new System.Drawing.Size(1276, 532);
-            this.tabPage5.TabIndex = 2;
-            this.tabPage5.Text = "реальное";
-            // 
-            // tabPage6
-            // 
-            this.tabPage6.Controls.Add(this.label9);
-            this.tabPage6.Location = new System.Drawing.Point(4, 26);
-            this.tabPage6.Name = "tabPage6";
-            this.tabPage6.Size = new System.Drawing.Size(1276, 532);
-            this.tabPage6.TabIndex = 3;
-            this.tabPage6.Text = "3d планировака";
-            // 
-            // tabPage7
-            // 
-            this.tabPage7.Location = new System.Drawing.Point(4, 26);
-            this.tabPage7.Name = "tabPage7";
-            this.tabPage7.Size = new System.Drawing.Size(1276, 532);
-            this.tabPage7.TabIndex = 4;
-            this.tabPage7.Text = "логотип";
-            // 
-            // tabPage8
-            // 
-            this.tabPage8.Location = new System.Drawing.Point(4, 26);
-            this.tabPage8.Name = "tabPage8";
-            this.tabPage8.Size = new System.Drawing.Size(1276, 532);
-            this.tabPage8.TabIndex = 5;
-            this.tabPage8.Text = "спам АН";
-            // 
-            // tabPage9
-            // 
-            this.tabPage9.Location = new System.Drawing.Point(4, 26);
-            this.tabPage9.Name = "tabPage9";
-            this.tabPage9.Size = new System.Drawing.Size(1276, 532);
-            this.tabPage9.TabIndex = 6;
-            this.tabPage9.Text = "спам";
-            // 
-            // tabPage10
-            // 
-            this.tabPage10.Location = new System.Drawing.Point(4, 26);
-            this.tabPage10.Name = "tabPage10";
-            this.tabPage10.RightToLeft = System.Windows.Forms.RightToLeft.No;
-            this.tabPage10.Size = new System.Drawing.Size(1276, 532);
-            this.tabPage10.TabIndex = 7;
-            this.tabPage10.Text = "карта";
             // 
             // label7
             // 
@@ -311,6 +234,15 @@
             this.label7.TabIndex = 0;
             this.label7.Text = "Показано 0 из 0";
             // 
+            // tabPage5
+            // 
+            this.tabPage5.Controls.Add(this.label8);
+            this.tabPage5.Location = new System.Drawing.Point(4, 26);
+            this.tabPage5.Name = "tabPage5";
+            this.tabPage5.Size = new System.Drawing.Size(1136, 427);
+            this.tabPage5.TabIndex = 2;
+            this.tabPage5.Text = "реальное";
+            // 
             // label8
             // 
             this.label8.AutoSize = true;
@@ -320,22 +252,149 @@
             this.label8.TabIndex = 0;
             this.label8.Text = "Показано 0 из 0";
             // 
+            // tabPage6
+            // 
+            this.tabPage6.Controls.Add(this.label9);
+            this.tabPage6.Location = new System.Drawing.Point(4, 26);
+            this.tabPage6.Name = "tabPage6";
+            this.tabPage6.Size = new System.Drawing.Size(1136, 427);
+            this.tabPage6.TabIndex = 3;
+            this.tabPage6.Text = "3d планировака";
+            // 
             // label9
             // 
             this.label9.AutoSize = true;
-            this.label9.Location = new System.Drawing.Point(37, 377);
+            this.label9.Location = new System.Drawing.Point(28, 480);
             this.label9.Name = "label9";
-            this.label9.Size = new System.Drawing.Size(46, 17);
+            this.label9.Size = new System.Drawing.Size(115, 17);
             this.label9.TabIndex = 0;
-            this.label9.Text = "label9";
+            this.label9.Text = "Показано 0 из 0";
+            // 
+            // tabPage7
+            // 
+            this.tabPage7.Controls.Add(this.label10);
+            this.tabPage7.Location = new System.Drawing.Point(4, 26);
+            this.tabPage7.Name = "tabPage7";
+            this.tabPage7.Size = new System.Drawing.Size(1136, 427);
+            this.tabPage7.TabIndex = 4;
+            this.tabPage7.Text = "логотип";
+            // 
+            // label10
+            // 
+            this.label10.AutoSize = true;
+            this.label10.Location = new System.Drawing.Point(28, 496);
+            this.label10.Name = "label10";
+            this.label10.Size = new System.Drawing.Size(115, 17);
+            this.label10.TabIndex = 0;
+            this.label10.Text = "Показано 0 из 0";
+            // 
+            // tabPage8
+            // 
+            this.tabPage8.Controls.Add(this.label11);
+            this.tabPage8.Location = new System.Drawing.Point(4, 26);
+            this.tabPage8.Name = "tabPage8";
+            this.tabPage8.Size = new System.Drawing.Size(1136, 427);
+            this.tabPage8.TabIndex = 5;
+            this.tabPage8.Text = "спам АН";
+            // 
+            // label11
+            // 
+            this.label11.AutoSize = true;
+            this.label11.Location = new System.Drawing.Point(28, 495);
+            this.label11.Name = "label11";
+            this.label11.Size = new System.Drawing.Size(115, 17);
+            this.label11.TabIndex = 0;
+            this.label11.Text = "Показано 0 из 0";
+            // 
+            // tabPage9
+            // 
+            this.tabPage9.Controls.Add(this.label12);
+            this.tabPage9.Location = new System.Drawing.Point(4, 26);
+            this.tabPage9.Name = "tabPage9";
+            this.tabPage9.Size = new System.Drawing.Size(1136, 427);
+            this.tabPage9.TabIndex = 6;
+            this.tabPage9.Text = "спам";
+            // 
+            // label12
+            // 
+            this.label12.AutoSize = true;
+            this.label12.Location = new System.Drawing.Point(28, 459);
+            this.label12.Name = "label12";
+            this.label12.Size = new System.Drawing.Size(115, 17);
+            this.label12.TabIndex = 0;
+            this.label12.Text = "Показано 0 из 0";
+            // 
+            // tabPage10
+            // 
+            this.tabPage10.Controls.Add(this.label13);
+            this.tabPage10.Location = new System.Drawing.Point(4, 26);
+            this.tabPage10.Name = "tabPage10";
+            this.tabPage10.RightToLeft = System.Windows.Forms.RightToLeft.No;
+            this.tabPage10.Size = new System.Drawing.Size(1136, 427);
+            this.tabPage10.TabIndex = 7;
+            this.tabPage10.Text = "карта";
+            // 
+            // label13
+            // 
+            this.label13.AutoSize = true;
+            this.label13.Location = new System.Drawing.Point(28, 462);
+            this.label13.Name = "label13";
+            this.label13.Size = new System.Drawing.Size(115, 17);
+            this.label13.TabIndex = 0;
+            this.label13.Text = "Показано 0 из 0";
+            // 
+            // panel2
+            // 
+            this.panel2.Controls.Add(this.label14);
+            this.panel2.Controls.Add(this.label6);
+            this.panel2.Controls.Add(this.label5);
+            this.panel2.Controls.Add(this.progressBar1);
+            this.panel2.Location = new System.Drawing.Point(592, 22);
+            this.panel2.Name = "panel2";
+            this.panel2.Size = new System.Drawing.Size(642, 209);
+            this.panel2.TabIndex = 0;
+            // 
+            // label14
+            // 
+            this.label14.AutoSize = true;
+            this.label14.Location = new System.Drawing.Point(113, 167);
+            this.label14.Name = "label14";
+            this.label14.Size = new System.Drawing.Size(16, 17);
+            this.label14.TabIndex = 3;
+            this.label14.Text = "0";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(206, 92);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(13, 17);
+            this.label6.TabIndex = 2;
+            this.label6.Text = "-";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(113, 92);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(87, 17);
+            this.label5.TabIndex = 1;
+            this.label5.Text = "Распознано";
+            // 
+            // progressBar1
+            // 
+            this.progressBar1.Location = new System.Drawing.Point(113, 130);
+            this.progressBar1.Name = "progressBar1";
+            this.progressBar1.Size = new System.Drawing.Size(413, 23);
+            this.progressBar1.TabIndex = 0;
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 17F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.AutoSize = true;
-            this.ClientSize = new System.Drawing.Size(1324, 638);
-            this.Controls.Add(this.tabControl2);
+            this.ClientSize = new System.Drawing.Size(1324, 749);
+            this.Controls.Add(this.panel2);
             this.Controls.Add(this.panel1);
             this.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
             this.Margin = new System.Windows.Forms.Padding(4);
@@ -351,14 +410,22 @@
             this.tabControl2.ResumeLayout(false);
             this.tabPage3.ResumeLayout(false);
             this.tabPage3.PerformLayout();
-            this.panel2.ResumeLayout(false);
-            this.panel2.PerformLayout();
             this.tabPage4.ResumeLayout(false);
             this.tabPage4.PerformLayout();
             this.tabPage5.ResumeLayout(false);
             this.tabPage5.PerformLayout();
             this.tabPage6.ResumeLayout(false);
             this.tabPage6.PerformLayout();
+            this.tabPage7.ResumeLayout(false);
+            this.tabPage7.PerformLayout();
+            this.tabPage8.ResumeLayout(false);
+            this.tabPage8.PerformLayout();
+            this.tabPage9.ResumeLayout(false);
+            this.tabPage9.PerformLayout();
+            this.tabPage10.ResumeLayout(false);
+            this.tabPage10.PerformLayout();
+            this.panel2.ResumeLayout(false);
+            this.panel2.PerformLayout();
             this.ResumeLayout(false);
 
         }
@@ -392,6 +459,11 @@
         private System.Windows.Forms.Label label7;
         private System.Windows.Forms.Label label8;
         private System.Windows.Forms.Label label9;
+        private System.Windows.Forms.Label label10;
+        private System.Windows.Forms.Label label11;
+        private System.Windows.Forms.Label label12;
+        private System.Windows.Forms.Label label13;
+        private System.Windows.Forms.Label label14;
     }
 }
 
--- EsepteImage/Form1.cs
+++ EsepteImage/Form1.cs
@@ -23,6 +23,7 @@ namespace EsepteImage
 
         int[] tabsImages = new int[8];
 
+        int maxPagination = 100;
 
         private static Lazy<PredictionEngine<ModelInput, ModelOutput>> PredictionEngine = null;
         public static string MLNetModelPath = Path.GetFullPath("MachineLearningNoticeProperty/ntsprmdl.zip");
@@ -35,6 +36,8 @@ namespace EsepteImage
             PredictionEngine = new Lazy<PredictionEngine<ModelInput, ModelOutput>>(predEngine);
 
             InitializeComponent();
+
+            panel2.Visible = false;
         }
 
         public static ModelOutput Predict(ModelInput input)
@@ -123,97 +126,181 @@ namespace EsepteImage
         private void button1_Click(object sender, EventArgs e)
         {
             panel1.Visible = false;
+            panel2.Visible = true;
             progressBar1.Value = 1;
+            tabControl2.Visible = false;
 
             int x = 0;
             int y = 0;
             int i = 0;
             int n = 0;
+            string l = "/" + filesPaths.Length;
 
             ImageTypes = new List<ImageType>();
 
             foreach (string filePath in filesPaths)
             {
-                string result = Recognize(filePath);
+                i++;
+                n++;
+                label14.Text = n + l;
+                progressBar1.Value = (int)GetProcent(n);
 
+                // reco
+                string result = Recognize(filePath);
                 label6.Text = GetTypeRu(result);
 
-                n++;
-                if (n < 100)
-                {
-                    i++;
+                // save
+                ImageTypes.Add(new ImageType { ImagePath = filePath, Prediction = result });
 
-                    var pctBox = new PictureBox();
-                    pctBox.SizeMode = PictureBoxSizeMode.StretchImage;
-                    pctBox.Image = Image.FromFile(filePath);
-                    pctBox.Location = new Point(x, y);
-
-                    pctBox.Width = 250;
-                    pctBox.Height = 220;
-                    x += pctBox.Width + 2;
-                    if (i > 5)
-                    {
-                        i = 0;
-                        y += pctBox.Height + 2;
-                        x = 0;
-                    }
-                    ImageTypes.Add(new ImageType { ImagePath = filePath, Prediction = result });
-                    AddToTab(pctBox, result);
-                }
+                AddToTab(result, filePath, x, i, y);
             }
 
             tabControl2.Visible = true;
-            label4.Visible = true;
+            panel2.Visible = false;
+
+            // info
+            label13.Text = GetPagination(tabsImages[7]);
+            label12.Text = GetPagination(tabsImages[6]);
+            label11.Text = GetPagination(tabsImages[5]);
+            label10.Text = GetPagination(tabsImages[4]);
+            label9.Text = GetPagination(tabsImages[3]);
+            label8.Text = GetPagination(tabsImages[2]);
+            label7.Text = GetPagination(tabsImages[1]);
+            label1.Text = GetPagination(tabsImages[0]);
         }
 
-        private void AddToTab(PictureBox pctBox, string result)
+        private double GetProcent(int v)
         {
+            return (100 * v) / filesPaths.Length;
+        }
+
+        private string GetPagination(int num)
+        {
+            if (num > 99)
+            {
+                return "Показаны " + 100 + " из " + num;
+            }
+            return "Показаны " + num + " из " + num;
+        }
+
+        private void AddToTab(string result, string filePath, int x, int i, int y)
+        {
+
+            PictureBox pctBox = null;
 
             switch (result)
             {
                 case "2d_plan":
                     tabsImages[0]++;
+                    if (tabsImages[0] > maxPagination)
+                    {
+                        break;
+                    }
+                    pctBox = GetPctBox(filePath, x, i, y);
                     tabPage3.Controls.Add(pctBox);
                     break;
                 case "2d_plan_photo":
                     tabsImages[1]++;
+                    if (tabsImages[1] > maxPagination)
+                    {
+                        break;
+                    }
+                    pctBox = GetPctBox(filePath, x, i, y);
                     tabPage4.Controls.Add(pctBox);
                     break;
                 case "3d_plan":
                     tabsImages[3]++;
+                    if (tabsImages[3] > maxPagination)
+                    {
+                        break;
+                    }
+                    pctBox = GetPctBox(filePath, x, i, y);
                     tabPage6.Controls.Add(pctBox);
                     break;
                 case "artwork":
                     tabsImages[4]++;
+                    if (tabsImages[4] > maxPagination)
+                    {
+                        break;
+                    }
+                    pctBox = GetPctBox(filePath, x, i, y);
                     tabPage7.Controls.Add(pctBox);
                     break;
                 case "desk_bred_property":
                     tabsImages[5]++;
+                    if (tabsImages[5] > maxPagination)
+                    {
+                        break;
+                    }
+                    pctBox = GetPctBox(filePath, x, i, y);
                     tabPage8.Controls.Add(pctBox);
                     break;
                 case "desk_bred_text_and_photo":
                     tabsImages[6]++;
+                    if (tabsImages[6] > maxPagination)
+                    {
+                        break;
+                    }
+                    pctBox = GetPctBox(filePath, x, i, y);
                     tabPage9.Controls.Add(pctBox);
                     break;
                 case "document":
                     tabsImages[1]++;
+                    if (tabsImages[1] > maxPagination)
+                    {
+                        break;
+                    }
+                    pctBox = GetPctBox(filePath, x, i, y);
                     tabPage4.Controls.Add(pctBox);
                     break;
                 case "map":
                     tabsImages[7]++;
+                    if (tabsImages[7] > maxPagination)
+                    {
+                        break;
+                    }
+                    pctBox = GetPctBox(filePath, x, i, y);
                     tabPage10.Controls.Add(pctBox);
                     break;
                 case "poster":
                     tabsImages[6]++;
+                    if (tabsImages[6] > maxPagination)
+                    {
+                        break;
+                    }
+                    pctBox = GetPctBox(filePath, x, i, y);
                     tabPage9.Controls.Add(pctBox);
                     break;
                 case "real":
                     tabsImages[2]++;
+                    if (tabsImages[2] > maxPagination)
+                    {
+                        break;
+                    }
+                    pctBox = GetPctBox(filePath, x, i, y);
                     tabPage5.Controls.Add(pctBox);
                     break;
                 default:
                     break;
             }
+        }
+
+        private PictureBox GetPctBox(string filePath, int x, int i, int y)
+        {
+            var pctBox = new PictureBox();
+            pctBox.SizeMode = PictureBoxSizeMode.StretchImage;
+            pctBox.Image = Image.FromFile(filePath);
+            pctBox.Location = new Point(x, y);
+            pctBox.Width = 250;
+            pctBox.Height = 220;
+            x += pctBox.Width + 2;
+            if (i > 5)
+            {
+                i = 0;
+                y += pctBox.Height + 2;
+                x = 0;
+            }
+            return pctBox;
         }
 
         private void Form1_Load(object sender, EventArgs e)
